48e3c1ad0c1a93cccba568b1366ce4127b64ce0a
tteenz <trantiinaa@yahoo.com>
2019-03-12 21:08:37 -0700

    updates

--- models/Article.js
+++ models/Article.js
@@ -1,17 +1,13 @@
+// Require mongoose
 var mongoose = require("mongoose");
-
-// Save a reference to the Schema constructor
 var Schema = mongoose.Schema;
 
-// Using the Schema constructor, create a new UserSchema object
-// This is similar to a Sequelize model
+// Create Schema
 var ArticleSchema = new Schema({
-  // `title` is required and of type String
   title: {
     type: String,
     required: true
   },
-  // `link` is required and of type String
   link: {
     type: String,
     required: true
@@ -20,17 +16,13 @@ var ArticleSchema = new Schema({
     type: Boolean,
     default: false
   },
-  // `note` is an object that stores a Note id
-  // The ref property links the ObjectId to the Note model
-  // This allows us to populate the Article with an associated Note
-  note: {
+  notes: [{
     type: Schema.Types.ObjectId,
     ref: "Note"
-  }
+  }]
 });
 
-// This creates our model from the above schema, using mongoose's model method
 var Article = mongoose.model("Article", ArticleSchema);
 
-// Export the Article model
+// Export the model
 module.exports = Article;
--- models/Note.js
+++ models/Note.js
@@ -1,18 +1,14 @@
-var mongoose = require("mongoose");
 
-// Save a reference to the Schema constructor
+// Require mongoose
+var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
-// Using the Schema constructor, create a new NoteSchema object
-// This is similar to a Sequelize model
 var NoteSchema = new Schema({
-  // `title` is of type String
-  title: String,
-  // `body` is of type String
-  body: String
+  body: {
+    type: String
+  }
 });
 
-// This creates our model from the above schema, using mongoose's model method
 var Note = mongoose.model("Note", NoteSchema);
 
 // Export the Note model
