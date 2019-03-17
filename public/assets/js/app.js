fc4f7be16fe0ee9edf9953315bddaf1161712cbc
tteenz <trantiinaa@yahoo.com>
2019-03-16 22:17:14 -0700

    res

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,9 +1,3 @@
-$.getJSON("articles", function(data) {
-  for (var i = 0; i <data.length; i++) {
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
-  }
-});
-
 $(document).ready(function () {
   $(".save-btn").on("click", function (event) {
     var newSavedArticle = $(this).data();
